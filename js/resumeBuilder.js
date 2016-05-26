9a89a4ffd627b39c8380564807cca6a8967427e3
Max Ivannnikov <Max Ivannnikov>
2016-05-26 00:37:58 -0400

    uodates to my resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,33 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+"name" : "Max Ivannikov",
+"role" : "aspiring WebDeveloper",
+"skills" : [
+"HTML5", "CSS", "JavaScript", "jQuery", "Bootstrap"
+],
+"contacts" : {
+	"email" : "mvi205@nyu.edu",
+	"mobile" : "646-785-0001"
+},
+"bioPic": ""
+}
+
+var work = {
+	"title":"postdoc"
+}
+work.dates ="2013 - present";
+work.employer = "NYU Medical Center";
+work. location = "New York, NY";
+
+var education = {
+	"degrees": [
+{"degree":"PhD",
+  "major" : "Biomedical Sciences",
+  "University": "NYU"
+	},
+{"degree":"MS",
+  "major" : "Biochemistry",
+  "University": "NSU"}
+  ]
+}
+ 
+var projects ={ }
